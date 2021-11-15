34a210caa1f585f26efb2667c7ab74c9f6b03326
masterflo1512 <sakharovbig@gmail.com>
2021-11-15 19:35:00 +0200

    added component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchComponent } from './search/search.component';
+import { FilterComponent } from './filter/filter.component';
+import { PaginationComponent } from './pagination/pagination.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SearchComponent,
+    FilterComponent,
+    PaginationComponent,
+    ProductListComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
